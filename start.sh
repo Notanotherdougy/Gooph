1c8eada656736f57fd51adc1b1953c2452279d89
Randomryan <152279228+Notanotherdougy@users.noreply.github.com>
2024-06-19 15:32:15 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,14 @@
 # Ensure necessary directories are created
 mkdir -p .tunnels_log
 
-# Generate a random port between 2000 and 65000
-RANDOM_PORT=$(( ( RANDOM % 63000 ) + 2000 ))
+# Generate a random port between 8000 and 9000
+RANDOM_PORT=$(( ( RANDOM % 1000 ) + 8000 ))
 
 # Start the PHP server on the random port
 cd .www && php -S 127.0.0.1:$RANDOM_PORT > /dev/null 2>&1 &
+
+# Save the random port to a log file for later use
+echo $RANDOM_PORT > .tunnels_log/port.log
 
 # Start the Cloudflared tunnel
 echo -ne "\nStarting Cloudflared on port $RANDOM_PORT..."
